aa01380d0070173fc2eb8c8dec8bda6f4efe7cc9
unknown <s65199@beuth-hochschule.de>
2015-12-14 18:59:18 +0100

    initial push

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour {
+
+	Transform thistransform;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		thistransform = transform;
+		health = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thistransform.Translate (new Vector3 (-0.3f, 0, 0));
+	}
+
+	public void onDeath(){
+		Debug.Log("tot");
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Tower1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower1 : MonoBehaviour {
+
+	Transform thistransform;
+
+	// Use this for initialization
+	void Start () {
+		thistransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		Collider2D[] coll = Physics2D.OverlapCircleAll (transform.position, 3f);
+		foreach (Collider2D c in coll) {
+			//Debug.Log("feind");
+
+			if(c.name.Equals("enemy")){
+
+				CreateProjectile cp = GetComponent<CreateProjectile> ();
+				cp.create ();
+
+				Enemy1 script = c.GetComponent<Enemy1> ();
+				if(script != null){
+					script.health -= 1;
+				if(script.health <= 0){
+					script.onDeath ();
+				}
+				}
+			}
+		}
+	
+	}
+}
