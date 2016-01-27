a40f5beb890ab9ab063fc6caa68098a627b3ddd4
Lydia Selene Fahle <lydia.fahle@gmx.de>
2016-01-27 11:55:07 +0100

    Erste Wegpunkte-Version

--- Assets/Scripts/Enemies/Enemy_Dragon.cs
+++ Assets/Scripts/Enemies/Enemy_Dragon.cs
@@ -1,20 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy_Dragon : MonoBehaviour {
 
 	Transform thistransform;
 	float health;
 	float movingSpeed;
+	Vector3 targetPoint;
 	Vector3 movingDirection;
+	//List<Vector3> waypoints;
+	List<Vector3> waypoints;
+	int wayPointNumber;
 
 	// Use this for initialization
 	void Start () {
 		thistransform = transform;
 		health = 10;
 		movingSpeed = 7.0f;
+		targetPoint = transform.position;
+	}
 
-		movingDirection = new Vector3 (-1, 0, 0);
+	public void setWaypoints(List<Vector3> list){
+		waypoints = list;
+		wayPointNumber = 0;
+	}
+
+	void setNextWaypoint(){
+		//Debug.Log ("wayPointNumber " + wayPointNumber);
+		if (wayPointNumber < waypoints.Count) {
+			targetPoint = waypoints [wayPointNumber];
+			//Debug.Log ("targetPoint " + targetPoint);
+			wayPointNumber += 1;
+			//waypoints.RemoveAt (0);
+		} else {
+			targetPoint = thistransform.position;
+			movingDirection = Vector3.zero;
+		}
 	}
 
 	public void dealDamage(float amount){
@@ -25,7 +47,6 @@ public class Enemy_Dragon : MonoBehaviour {
 	}
 
 	public void onDeath(){
-		Debug.Log("tot");
 		Destroy (gameObject);
 	}
 
@@ -36,7 +57,12 @@ public class Enemy_Dragon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		thistransform.Translate (movingDirection*movingSpeed*Time.deltaTime, Space.World);
+
+		if(Vector3.Distance(thistransform.position, targetPoint) < 0.15f){
+			setNextWaypoint ();
+		}
+		movingDirection = targetPoint - thistransform.position;
+		thistransform.Translate (movingDirection.normalized*movingSpeed*Time.deltaTime, Space.World);
 	}
 
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour {
 
 	public GameObject monsterPrefab;
 	float spawnTimer;
+	List<Vector3> waypoints;
 
 	// Use this for initialization
 	void Start () {
 	
 		spawnTimer = 2.0f;
+		waypoints = new List<Vector3> ();
+		Transform wayPointsChild = transform.GetChild (0);
+		for (int i = 0; i < wayPointsChild.childCount; i++) {
+			waypoints.Add(wayPointsChild.GetChild(i).transform.position);
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -20,7 +28,8 @@ public class Spawner : MonoBehaviour {
 			GameObject m = GameObject.Instantiate (monsterPrefab);
 			m.transform.position = transform.position;
 			m.transform.rotation = Quaternion.identity;
-			spawnTimer = 2.0f;
+			m.SendMessage ("setWaypoints", waypoints, SendMessageOptions.RequireReceiver );
+			spawnTimer = 3.0f;
 		}
 	}
 }
